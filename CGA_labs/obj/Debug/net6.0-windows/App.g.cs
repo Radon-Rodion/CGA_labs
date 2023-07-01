085acae353681a8e3bba8292da8e0608249400a2
pavel.rafeev <pavel.rafeev@b1.by>
2023-07-01 08:58:06 +0300

    saved materials before removing local project

--- CGA_labs/obj/Debug/net6.0-windows/App.g.cs
+++ CGA_labs/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace CGA_labs {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CGA_labs {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             CGA_labs.App app = new CGA_labs.App();
             app.InitializeComponent();
